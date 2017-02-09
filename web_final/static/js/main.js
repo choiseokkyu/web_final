916d928c5f534f50b72eb3614b4102d812e294ea
Choi Seokkyu <korest75@gmail.com>
2017-02-09 17:46:35 +0900

    'hi

---
+++ web_final/static/js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by umhyunsik on 2017. 1. 25..
+ */
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
+$('button').click(function(){
+    $("table input[type='checkbox']:checked").parent().parent().remove();
+});
