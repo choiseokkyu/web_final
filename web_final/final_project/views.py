2e0ca3ab31b2f8f62b3d0688a48529e05845f551
Choi Seokkyu <korest75@gmail.com>
2017-02-09 19:21:19 +0900

    'first_upload'

--- web_final/final_project/views.py
+++ web_final/final_project/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def introduce(request):
+    introduce =
+    return render(request, main.html {'button': introduce})
