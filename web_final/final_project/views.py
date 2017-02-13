b7e1f3a5272bffd54a16da6bef79bbce8ef0b3c3
Umhyunsik <uhs2000@naver.com>
2017-02-13 12:36:17 +0900

    first

--- web_final/final_project/views.py
+++ web_final/final_project/views.py
@@ -2,6 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def introduce(request):
-    introduce =
-    return render(request, main.html {'button': introduce})
+def main(request):
+    return render(request, "main.html")
+def about(request):
+    return render(request, "about.html")
+def contact(request):
+    return render(request,"contact.html")
+def project(request):
+    return render(request,"main.html")
+def project1(request):
+    return render(request,"project1.html")
+def project2(request):
+    return render(request,"project2.html")
+def project3(request):
+    return render(request,"project3.html")
+def project4(request):
+    return render(request,"project4.html")
+def project5(request):
+    return render(request,"project5.html")
+def project6(request):
+    return render(request,"project6.html")
+def project7(request):
+    return render(request,"project7.html")
+def project8(request):
+    return render(request,"project8.html")
+def project9(request):
+    return render(request,"project9.html")
